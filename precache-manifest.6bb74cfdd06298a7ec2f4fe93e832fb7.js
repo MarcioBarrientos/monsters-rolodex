de6b32fd21f09aeb4222821f1c76fe4d51909e90
Marcio Barrientos <marcio.barrientos@unah.edu.hn>
2020-06-05 13:21:33 -0600

    Updates

---
+++ precache-manifest.6bb74cfdd06298a7ec2f4fe93e832fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd6dba225239f105bfa802d66f98d97",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "46ee71ce3927bfe323e1",
+    "url": "/monsters-rolodex/static/css/main.09bed652.chunk.css"
+  },
+  {
+    "revision": "9cb317d23bd6aa35d016",
+    "url": "/monsters-rolodex/static/js/2.70aa4132.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.70aa4132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ee71ce3927bfe323e1",
+    "url": "/monsters-rolodex/static/js/main.f8a1f098.chunk.js"
+  },
+  {
+    "revision": "0beb9e6ed4496955c123",
+    "url": "/monsters-rolodex/static/js/runtime-main.f44d22cf.js"
+  }
+]);
